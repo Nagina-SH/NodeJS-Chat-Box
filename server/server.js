69899695a0b312ab2c8162b3563b2e23657e829f
unknown <shailu.nagina@gmail.com>
2017-09-03 09:31:17 +0530

    ini commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 0888;
 
 
 // console.log(__dirname + '/../public');
@@ -12,6 +12,6 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-	console.log(`server up and running on port ${port}`);
+app.listen(port, () => {
+	console.log(`server up and running on ${port}`);
 })
